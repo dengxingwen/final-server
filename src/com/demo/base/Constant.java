3c429bf904502583c48323cde9769bbd3902798c
markcc <970518209@qq.com>
2015-12-16 09:20:30 +0800

    11

--- src/com/demo/base/Constant.java
+++ src/com/demo/base/Constant.java
@@ -8,6 +8,6 @@ public class Constant {
 	
 	public static final String KEY = "vnroth0krcb1o";//第三方推送平台的key和secret
 	public static final String SECRET = "OMTeUvRCdL";
-	public static final String SERVER_URL = "http://192.168.0.117";
+	public static final String SERVER_URL = "http://192.168.0.117"; //
 
 }
